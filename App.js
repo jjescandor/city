0365976e1c6af73a56219b4ca231027a393dcc73
JJ Escandor <jj.p.escandor@gmail.com>
2022-03-16 19:29:12 -0400

    added styling

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+
 import axios from 'axios';
 
 class App extends React.Component {
--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchQuery: "",
-      locationObj: ""
+      locationObj: "",
+      mapResponse: ""
     }
   }
 
   getLocation = async () => {
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
     const locationResponse = await axios.get(url);
-    console.log(locationResponse.data[0].display_name);
-    console.log(locationResponse.data[0].lat);
-    console.log(locationResponse.data[0].lon);
     this.setState({ locationObj: locationResponse.data[0] });
+    this.getMap();
+  }
+
+  getMap = async () => {
+    const mapUrlTwo = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=${13}&size=${300}x${500}&format=jpeg&maptype=<MapType>&markers=icon:large-purple-cutout|${this.state.locationObj.lat},${this.state.locationObj.lon}&markers=icon:large-purple-cutout|${this.state.locationObj.lat},${this.state.locationObj.lon}`;
+    const mapResponse = await axios.get(mapUrlTwo);
+    console.log(mapResponse.data);
+    this.setState({ mapResponse: mapResponse.config.url });
   }
   render() {
     return (
@@ -27,11 +34,12 @@ class App extends React.Component {
         }} />
         <button onClick={this.getLocation} >Click Me</button>
         {this.state.locationObj.display_name &&
-          <>
+          <Card className='cityCard'>
             <h2>{this.state.locationObj.display_name}</h2>
-            <h3>Latitude: {this.state.locationObj.lat}</h3>
-            <h3>Longitude: {this.state.locationObj.lon}</h3>
-          </>
+            <img src={this.state.mapResponse} alt="this" />
+            <h3>Latitude: {parseInt(this.state.locationObj.lat)}</h3>
+            <h3>Longitude: {parseInt(this.state.locationObj.lon)}</h3>
+          </Card>
         }
       </div>
     )
