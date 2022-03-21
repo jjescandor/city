6a0f1ef8b9f835200549b22042b105aee4236ed4
JJ Escandor <jj.p.escandor@gmail.com>
2022-03-20 21:46:57 -0400

    Added API error modal

--- src/APIerr.js
+++ src/APIerr.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './APIerr.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { findAllByTestId } from '@testing-library/react';
 
 class APIerr extends React.Component {
     render() {
+
         return (
-            <div className='errContainer'>
-                {this.props.APIerror &&
-                    <h1 className="errMsg">{this.props.APIerror}</h1>}
-                {this.props.weatherResponseErr &&
-                    <h1 className="errMsg">{this.props.weatherResponseErr}</h1>}
-            </div>
+            // <div className='errContainer'>
+            //     {this.props.APIerror &&
+            //         <h1 className="errMsg">{this.props.APIerror}</h1>}
+            //     {this.props.weatherResponseErr &&
+            //         <h1 className="errMsg">{this.props.weatherResponseErr}</h1>}
+            // </div>
+            <>
+                {/* {this.props.APIerror && */}
+                <Modal show={this.props.errShow}>
+                    <Modal.Body>
+                        <p>{this.props.APIerror}</p>
+                        <p>{this.props.weatherResponseErr}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.props.handleErrClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
         );
     }
 }
